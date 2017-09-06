c0ed6053242d6c66317871b849808c631068df20
Leena S N <leenasn@gmail.com>
2017-09-06 12:19:50 +0530

    Sample app integrated with react-native-sound

--- android/app/src/main/java/com/sampleapp/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/sampleapp/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.sampleapp.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expcaa01b2361ee4bd39d2139d200597f40://tf-3wz.leenasn.sample-project.exp.direct:80";
 
 }
